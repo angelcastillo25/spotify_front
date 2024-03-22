e0591ebf58c34c4bd56831331cd8bb1fa528370b
Daniel <danyochoa112@gmail.com>
2024-03-22 10:05:52 -0600

    pasos register terminados, sin javascript

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -17,6 +17,7 @@
         <div id="register_container">
             <h1 class="title">Registrate para empezar a escuchar musica gratis</h1>
         <form action="POST" class="form" id="register_form">
+            <!-- Paso 1 -->
             <div class="step" id="step1">
                 <div class="form_section">
                         <label for="">Correo Electronico</label>
@@ -31,34 +32,36 @@
                     <a href="">Inicia sesion aqui</a>
                 </div>
             </div>
+            <!-- Paso 2 -->
             <div class="step" id="step2">
                 <div class="bars">
                     <div class="step_bar"></div>
-                    <div class="step_bar" id="step_bar_green"></div>
+                    <div class="step_bar_green"></div>
                 </div>
-                <div style="width: 100%; margin:20px 0;">
+                <div class="back_section">
                     <div class="back_container">
                         <img src="img/backarrow.png" alt="icono de flecha atras" class="icon">
                         <div class="back_container_text">
                             <p>Paso 1 de 3</p>
-                            <p style="font-weight: 600;">Crea una contrasena</p>
+                            <p class="bold">Crea una contraseña</p>
                         </div>
                     </div>
                 </div>
                 <div class="form_section">
-                        <label for="">Contrasena</label>
-                        <input type="password" placeholder="Contrasena">
+                        <label for="">Contraseña</label>
+                        <input type="password" placeholder="Contraseña">
                 </div>
                 <button class="form_button">
                     Siguiente
                 </button>
             </div>
+            <!-- Paso 3 -->
             <div class="step" id="step3">
                 <div class="bars">
                     <div class="step_bar"></div>
-                    <div class="step_bar" id="step_bar_green"></div>
+                    <div class="step_bar_green"></div>
                 </div>
-                <div style="width: 100%; margin:20px 0;">
+                <div class="back_section">
                     <div class="back_container">
                         <img src="img/backarrow.png" alt="icono de flecha atras" class="icon">
                         <div class="back_container_text">
@@ -70,16 +73,40 @@
                 <div class="form_section">
                     <label for="">
                         <p class="label_name">Nombre</p>
-                        <p class="label_description">Hablanos de ti</p>
+                        <p class="label_description">Este nombre aparecerá en tu perfil</p>
                     </label>
                     <input type="password" placeholder="Contrasena">
                 </div>
                 <div class="form_section">
                     <label for="">
                         <p class="label_name">Fecha de nacimiento</p>
-                        <p class="label_description">Cuentanos que dia naciste</p>
+                        <p class="label_description">Cuentanos que día naciste</p>
                     </label>
-                    <input type="password" placeholder="Contrasena">
+                    <input type="date" class="date_input">
+                </div>
+                <div class="row_form_section">
+                    <div class="form_section">
+                        <label for="">
+                            <p class="label_name">Genero</p>
+                            <p class="label_description">Nada mas por chismear</p>
+                        </label>
+                        <select name="genero" id="genero_select">
+                            <option value="M">Masculino</option>
+                            <option value="F">Femenino</option>
+                            <option value="O">Otro</option>
+                        </select>
+                    </div>
+                    <div class="form_section">
+                        <label for="">
+                            <p class="label_name">Pais</p>
+                            <p class="label_description">¿De donde eres?</p>
+                        </label>
+                        <select name="genero" id="genero_select">
+                            <option value="H">Honduras</option>
+                            <option value="V">Venezuela</option>
+                            <option value="CR">Costa Rica</option>
+                        </select>
+                    </div>
                 </div>
                 <button class="form_button">
                     Siguiente
