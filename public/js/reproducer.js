b2084b1bc91338b765e4a7ce2f158807ff05316b
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2024-04-07 22:58:19 -0600

    Vista de perfil Terminada

--- public/js/reproducer.js
+++ public/js/reproducer.js
@@ -13,3 +13,82 @@ function changeImage() {
         image.src = image1;
     }
 }
+
+
+// Función para seleccionar un botón y cambiar sus estados
+function selectButton(button) {
+    // Deseleccionar todos los botones
+
+    let button_home = document.querySelector('#home');
+    let button_search = document.querySelector('#search');
+    let button_library = document.querySelector('#library');
+
+    button_home.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="40px" height="40px">
+        <path d="M 23.951172 4 A 1.50015 1.50015 0 0 0 23.072266 4.3222656 L 8.859375 15.519531 
+        C 7.0554772 16.941163 6 19.113506 6 21.410156 L 6 40.5 C 6 41.863594 7.1364058 43 8.5 
+        43 L 18.5 43 C 19.863594 43 21 41.863594 21 40.5 L 21 30.5 C 21 30.204955 21.204955 30 
+        21.5 30 L 26.5 30 C 26.795045 30 27 30.204955 27 30.5 L 27 40.5 C 27 41.863594 28.136406 
+        43 29.5 43 L 39.5 43 C 40.863594 43 42 41.863594 42 40.5 L 42 21.410156 C 42 19.113506 
+        40.944523 16.941163 39.140625 15.519531 L 24.927734 4.3222656 A 1.50015 1.50015 0 0 0 
+        23.951172 4 z M 24 7.4101562 L 37.285156 17.876953 C 38.369258 18.731322 39 20.030807 
+        39 21.410156 L 39 40 L 30 40 L 30 30.5 C 30 28.585045 28.414955 27 26.5 27 L 21.5 27 C 
+        19.585045 27 18 28.585045 18 30.5 L 18 40 L 9 40 L 9 21.410156 C 9 20.030807 9.6307412 
+        18.731322 10.714844 17.876953 L 24 7.4101562 z"/>
+        </svg>
+        <p>Inicio</p>
+    `;
+    button_search.innerHTML = `
+        <img src="img/search.png" alt="" class="img-bar">
+        <p>Buscar</p>
+    `;
+
+    button_library.innerHTML = `
+        <img src="img/library.png" alt="" class="img-bar">
+        <p>Bibloteca</p>
+    `;
+
+    // Seleccionar el botón actual
+    button.classList.add('selected');
+
+    // Cambiar el contenido del botón según su ID
+    const buttonId = button.id;
+    switch (buttonId) {
+        case 'home':
+            // Cambiar el SVG por otro SVG
+            button.innerHTML = `
+                <svg id="homeViewButton" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                    viewBox="0,0,256,256" width="40px" height="40px">
+                    <g fill="#ffffff" fill-rule="nonzero" 
+                        stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" 
+                        stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" 
+                        font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                        <g transform="scale(5.33333,5.33333)">
+                            <path d="M39.5,43h-9c-1.381,0 -2.5,-1.119 -2.5,
+                                -2.5v-9c0,-1.105 -0.895,-2 -2,-2h-4c-1.105,0 -2,0.895 -2,2v9c0,1.381 -1.119,2.5 -2.5,
+                                2.5h-9c-1.381,0 -2.5,-1.119 -2.5,-2.5v-19.087c0,-2.299 1.054,-4.471 2.859,-5.893l14.212,
+                                -11.199c0.545,-0.428 1.313,-0.428 1.857,0l14.214,11.199c1.805,1.422 2.858,3.593 2.858,
+                                5.891v19.089c0,1.381 -1.119,2.5 -2.5,2.5z">
+                            </path>
+                        </g>
+                    </g>
+                </svg>
+                <p>Inicio</p>
+            `;
+            break;
+        case 'search':
+            button.innerHTML = `
+                <img src="img/searchSelected.png" alt="" class="img-bar">
+                <p>Buscar</p>
+            `;
+            break;
+        case 'library':
+            button.innerHTML = `
+                <img src="img/librarySelected.png" alt="" class="img-bar">
+                <p>Bibloteca</p>
+            `;
+            break;
+        default:
+            break;
+    }
+}
--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -20,55 +20,56 @@
         <div class="list_header">
             <img class="back_button" src="img/backArrowMen.svg" alt="boton atras" id="back_btn">
             <div class="cover_container">
-                <img src="img/profil.jpg" class="cover_img" alt="portada list" id="list_cover">
+                <div class="cover-params">
+                    <img src="img/profil.jpg" class="cover_img" alt="portada list" id="list_cover">
+                    <div class="song_text_container">
+                        <p class="user-name">Angel Castillo</p>
+                        <p class="song_artist">11 seguidores • 23 siguiendo</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="options-menu">
+                <button class="button_header">Editar</button>
+                <img class="options_icon options_btn" src="img/option_points.svg" alt="Opciones">
             </div>
             
-            <div class="list_header_panel">
-            <p hidden id="list_name" class="list_name">Exitos Latinos</p>
-                <div class="play_button"><img class="play_icon" src="img/play.png" alt="boton pausa"  id="play_btn" data-state="0"></div>
-                <div class="description_container">
-                    <p>Los exitos de *Artista*, en un solo lugar. Dale play ahora!</p>
-                </div>
-                <div class="creator_container">
-                    <img src="img/profil.jpg" alt="usuario" class="profile_image">
-                    <p id="author_name">Daniel Ochoa</p>
-                </div>
-                <div class="description_container">
-                    <p>Se guardo 2,000 veces - 2h 34m</p>
-                </div>
-                <div class="buttons_container">
-                    <img class="list_button" src="img/descargar.svg" data-state="0" alt="" id="download_btn">
-                    <img class="list_button" src="img/agregar.svg" data-state="0" alt="" id="add_btn">
-                    <img class="options_icon" src="img/option_points.svg" alt="" id="options_btn">
-                </div>
+            <div style="padding: 10px 20px;">
+
+                <h1 class="user-name" style="font-size: 20px;">Playlist</h1>
+
             </div>
-        </div>
-        <div class="list_container">
-        <div class="list_item">
-                <div style="display:flex;">
-                    <img src="img/cardellino.jpeg" alt="" class="song_cover">
-                    <div class="song_text_container">
-                        <p class="song_title">Bambu</p>
-                        <p class="song_artist">Cardellino</p>
+
+            <div class="list_container">
+                <div class="list_item">
+                        <div style="display:flex;">
+                            <img src="img/cardellino.jpeg" alt="" class="song_cover">
+                            <div class="song_text_container" style="margin-left: 5px;">
+                                <p class="song_title">Clasicos</p>
+                                <p class="song_artist">Se guardo 0 veces</p>
+                            </div>
+                        </div>
+                </div>
+                <div class="list_item">
+                    <div style="display:flex;">
+                        <img src="img/badbunny.jpeg" alt="" class="song_cover">
+                        <div class="song_text_container" style="margin-left: 5px;">
+                            <p class="song_title">2016</p>
+                            <p class="song_artist">Se guardo 0 veces</p>
+                        </div>
                     </div>
                 </div>
-                <img class="options_icon options_btn" src="img/option_points.svg" alt="Opciones">
             </div>
-            <div class="list_item">
-                <div style="display:flex;">
-                    <img src="img/badbunny.jpeg" alt="" class="song_cover">
-                    <div class="song_text_container">
-                        <p class="song_title">Yonaguni</p>
-                        <p class="song_artist">Bad Bunny</p>
-                    </div>
-                </div>
-                <img class="options_icon options_btn" src="img/option_points.svg" alt="Opciones">
+
+            <div class="bottom-button">
+
+                <button class="button_header" style="margin-right: 0;">Ver todas las playlist</button>
+
             </div>
-        </div>
+
         <div class="superposition_background" id="superposition_background"></div>
-        <div class="option_menu" id="option_menu">
-            <div class="dragbar_container">
-                <div></div>
+            <div class="option_menu" id="option_menu">
+                    <div class="dragbar_container">
             </div>
             <div style="display:flex;">
                 <img src="img/cover.jpg" alt="" class="song_cover" id="cover_mini">
