e99d860edb7ecf36801452b5895c32e889954342
Daniel <danyochoa112@gmail.com>
2024-04-12 00:01:44 -0600

    menu de tres secciones terminado (podcast view)

--- public/js/podcast.js
+++ public/js/podcast.js
@@ -1,7 +1,76 @@
 import { changeIcon, showListOptions, hideOptionsMenu, slideOptionsMenu, endOptionsMenu} from "./functions.js";
 
-
 const notificationBtn = document.getElementById("notification_btn");
 
 notificationBtn.addEventListener('click', changeIcon.bind(notificationBtn,{1:"notificationOff.png", 0:"notificationOn.png"}))
 
+//Se inicializan los elementos del menu desplazable
+
+const episodesBtn = document.getElementById("episodes_btn");
+
+episodesBtn.addEventListener('click',()=>changeSection(1));
+
+const episodeSection = document.getElementById("episode_section");
+
+const aboutBtn = document.getElementById("about_btn");
+
+aboutBtn.addEventListener('click',()=>changeSection(2));
+
+const aboutSection = document.getElementById("about_section");
+
+const similarBtn = document.getElementById("similar_btn");
+similarBtn.addEventListener('click',()=>changeSection(3));
+
+const similarSection = document.getElementById("similar_section");
+
+initItems();
+
+function initItems(){
+    episodesBtn.style.fontWeight = "500"
+    episodesBtn.querySelector("div.green_line").style = "width:100%"
+    aboutSection.style = "position:fixed; right:-390px; top:360px; opacity:0"
+    similarSection.style = "position:fixed; right:-390px; top:360px; opacity:0"
+}
+
+function changeSection(elementIndex){
+
+    let hiddenStyles = "position:fixed; right:-390px; top:360px; opacity:0";
+
+    switch (elementIndex) {
+        case 1:
+            episodeSection.removeAttribute("style");
+            aboutSection.style = hiddenStyles;
+            similarSection.style = hiddenStyles;
+            episodesBtn.querySelector("div.green_line").style = "width:100%"
+            aboutBtn.querySelector("div.green_line").style = "margin-right:100%"
+            similarBtn.querySelector("div.green_line").style = "margin-right:100%"
+            episodesBtn.querySelector("p").style.fontWeight="500";
+            aboutBtn.querySelector("p").style.fontWeight="400";
+            similarBtn.querySelector("p").style.fontWeight="400";
+        break;
+        case 2:
+            aboutSection.removeAttribute("style");
+            episodeSection.style = hiddenStyles;
+            similarSection.style = hiddenStyles;
+            episodesBtn.querySelector("div.green_line").style = "margin-left:100%"
+            aboutBtn.querySelector("div.green_line").style = "width:100%"
+            similarBtn.querySelector("div.green_line").style = "margin-right:100%"
+            episodesBtn.querySelector("p").style.fontWeight="400";
+            aboutBtn.querySelector("p").style.fontWeight="500";
+            similarBtn.querySelector("p").style.fontWeight="400";
+        break;
+        case 3:
+            similarSection.removeAttribute("style");
+            aboutSection.style = hiddenStyles;
+            episodeSection.style = hiddenStyles;
+            episodesBtn.querySelector("div.green_line").style = "margin-left:100%"
+            aboutBtn.querySelector("div.green_line").style = "margin-left:100%"
+            similarBtn.querySelector("div.green_line").style = "width:100%"
+            episodesBtn.querySelector("p").style.fontWeight="400";
+            aboutBtn.querySelector("p").style.fontWeight="400";
+            similarBtn.querySelector("p").style.fontWeight="500";
+        break;
+        default:
+            break;
+    }
+}
--- resources/views/podcast.blade.php
+++ resources/views/podcast.blade.php
@@ -10,7 +10,9 @@
     <link rel="stylesheet" href="css/options_menu.css">
 </head>
 <body>
-    <header></header>
+    <header>
+    <img class="back_button" src="img/backArrowMen.svg" alt="boton atras" id="back_btn">
+    </header>
     <main>
         <div class="podcast_header">
             <img src="img/quantumfm.jpeg" alt="cover de podcast" class="podcast_cover">
@@ -28,24 +30,24 @@
             <img class="options_icon" src="img/option_points.svg" alt="" id="options_btn">
         </div>
         <div class="podcast_sections">
-            <div class="section_button">
+            <div class="section_button" id="episodes_btn">
                 <p>Episodios</p>
-                <div class="green_line" style="background-color: #1ed760;"></div>
+                <div class="green_line"></div>
             </div>
-            <div class="section_button">
+            <div class="section_button" id="about_btn">
                 <p>Acerca de</p>
                 <div class="green_line"></div>
             </div>
-            <div class="section_button">
+            <div class="section_button" id="similar_btn">
                 <p>Más contenido similar</p>
                 <div class="green_line"></div>
             </div>
         </div>
         <!-- Seccion de episodios !-->
-        <div class="episode_list">
+        <div class="episode_list" id="episode_section">
             <div class="episode_item">
                 <div class="title_container">
-                    <img src="img/cardellino.jpeg" alt="">
+                    <img src="img/quantumfm.jpeg" alt="">
                     <p>Albert Einstein, su Historia y su Ciencia Desconocida #11</p>
                 </div>
                 <p class="podcast_description">Albert Einstein fue un físico alemán de origen judío, nacionalizado después suizo, austriaco y estadounidense. Se le considera el científico más importante, conocido y popular del siglo XX.​​</p>
@@ -55,7 +57,60 @@
                     <img class="button_mini" src="img/agregar.svg" data-state="0" alt="" id="add_btn">
                     <img class="options_icon" src="img/option_points.svg" alt="" id="options_btn">
                 </div>
+                <div class="play_btn_container"><img src="img/play.png" alt="" data-state="0"></div>
                 <div class="separator"></div>
+            </div>
+            <div class="episode_item">
+                <div class="title_container">
+                    <img src="img/quantumfm.jpeg" alt="">
+                    <p>Albert Einstein, su Historia y su Ciencia Desconocida #11</p>
+                </div>
+                <p class="podcast_description">Albert Einstein fue un físico alemán de origen judío, nacionalizado después suizo, austriaco y estadounidense. Se le considera el científico más importante, conocido y popular del siglo XX.​​</p>
+                <p class="date_duration">9 nov 23 • 2h 13 min</p>
+                <div class="podcast_buttons">
+                    <img class="button_mini" src="img/descargar.svg" data-state="0" alt="" id="download_btn">
+                    <img class="button_mini" src="img/agregar.svg" data-state="0" alt="" id="add_btn">
+                    <img class="options_icon" src="img/option_points.svg" alt="" id="options_btn">
+                </div>
+                <div class="play_btn_container"><img src="img/play.png" alt="" data-state="0"></div>
+                <div class="separator"></div>
+            </div>
+            <div class="episode_item">
+                <div class="title_container">
+                    <img src="img/quantumfm.jpeg" alt="">
+                    <p>Albert Einstein, su Historia y su Ciencia Desconocida #11</p>
+                </div>
+                <p class="podcast_description">Albert Einstein fue un físico alemán de origen judío, nacionalizado después suizo, austriaco y estadounidense. Se le considera el científico más importante, conocido y popular del siglo XX.​​</p>
+                <p class="date_duration">9 nov 23 • 2h 13 min</p>
+                <div class="podcast_buttons">
+                    <img class="button_mini" src="img/descargar.svg" data-state="0" alt="" id="download_btn">
+                    <img class="button_mini" src="img/agregar.svg" data-state="0" alt="" id="add_btn">
+                    <img class="options_icon" src="img/option_points.svg" alt="" id="options_btn">
+                </div>
+                <div class="play_btn_container"><img src="img/play.png" alt="" data-state="0"></div>
+                <div class="separator"></div>
+            </div>
+        </div>
+        <!-- Seccion acerca de !-->
+        <div class="about_container" id="about_section">
+            <div class="about_btn_container">
+                <div class="about_btn"><p>5.0&nbsp;★&nbsp;(2k)</p></div>
+                <p class="separator_point">•</p>
+                <div class="about_btn"><p>Historia</p></div>
+                <div class="about_btn"><p>Entretenimiento</p></div>
+                <div class="about_btn"><p>Ciencia</p></div>
+            </div>
+            <p class="about_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat enim laudantium consectetur omnis consequuntur non atque tempora nulla dolor possimus. Doloribus necessitatibus quos rem quae architecto dicta sunt deserunt? Suscipit.</p>
+        </div>
+        <!-- Seccion Mas contenido similar !-->
+        <div class="similar_container" id="similar_section">
+            <div class="similar_item">
+                <img src="img/quantumfm.jpeg" alt="" class="similar_img">
+                <div class="similar_text">
+                    <p class="similar_gender">Ciencia • Politica • Educacion</p>
+                    <p class="similar_text">Coffe Break: Senal y Ruido</p>
+                    <p class="similar_description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti debitis in rem, ipsum, eaque fugit at vitae molestias quisquam a adipisci ea praesentium quis eos nulla minima.</p>
+                </div>
             </div>
         </div>
     </main>
