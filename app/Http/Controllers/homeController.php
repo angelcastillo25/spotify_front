fe9f9904b9923f53767d0628e8b6acca304aec39
angelcastillo25 <149858015+angelcastillo25@users.noreply.github.com>
2024-04-30 22:55:09 -0600

    homeController creado

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class homeController extends Controller
+{
+    public function getLibrary($idUser){
+        $client = new Client();
+
+        $response = $client->get('localhost:8080/home/inicio?id='.$idUser);
+
+        $home = json_decode($response->getBody());
+
+        return view('home', compact('home'));
+    }
+}
